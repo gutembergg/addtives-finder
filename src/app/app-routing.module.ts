0fec852c812e96565f1b4a90f1302640c1065b9e
gutembergg <gmascarenhas3001@gmail.com>
2021-04-16 11:50:14 +0200

    pwa3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
